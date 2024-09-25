da24161800475f4b9ea9bc3f91d475badbce6ebd
adrianql5 <adrianquiroga5@gmail.com>
2024-09-25 09:36:07 +0200

    Subidas Primeras Prácticas

---
+++ Practica1/CambioFormatoIPv4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h> //Librería para las funciones de red
+
+struct in_addr miip; //Struct tipo direccion IP en formato binario ej 0xC8806EC1
+
+int main( int argc, char ** argv){
+    //inter_pton, cambia formato de texto(193.110.128.200) a binario (0xC8806EC1)
+    if (inet_pton ( AF_INET , "193.110.128.200" , ( void * ) &miip ) != 1) {
+        //AF_INET (IPv4), char*src (cadena de texto), void* destino (struct)
+        fprintf ( stderr , "Formato de direccion incorrecto " ) ;
+        exit ( EXIT_FAILURE ) ;
+    }
+
+    //el formato uint8_t es como un array de 4 bytes diferentes, 
+    //y lo igualo al campo del struct que contiene la direccion
+    uint8_t* bytes = &miip.s_addr;
+    for (int i = 0; i < 4; i++){
+        printf(" %X", bytes[i]);
+    }
+
+    printf("\n");
+
+
+    //asigno el formato binario al campo del address
+    miip.s_addr = 0xC8806EC1;
+    //la direccion en formato tiene que tener una longitud maxima definida por INET_ADDRSTRLEN
+    char miiptext [INET_ADDRSTRLEN ] ;
+    //inet_ntop, cambia formato binario a texto
+    if ( inet_ntop ( AF_INET , (const void * ) &miip , miiptext , INET_ADDRSTRLEN) != NULL) {
+        //AF_INET(IPv4), void *src, char* dest(cadena de caracteres vacia), entero que indica el tamaño de bytes del destino
+        printf ( " %s \n" , miiptext ) ; 
+    }
+}
---
+++ Practica1/CambioFormatoIPv6.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+
+
+
+
+int main(int argc, char ** argv){
+    struct in6_addr ipv6;
+    int i=0;
+    char ip6[]="1080:0:0:0:8:800:200C:417A";
+    
+    //funcion que cambia de texto a binario
+    if ( inet_pton ( AF_INET6 , ip6, ( void * ) &ipv6 ) != 1) {
+        fprintf ( stderr , "Formato de direccion incorrecto " ) ;
+        exit( EXIT_FAILURE ) ;
+    }
+    //al ocupar el doble imprimo con un bucle
+    for(i; i<16;i++){
+        printf ( "%X" , ipv6.s6_addr[i]);
+    }
+    printf("\n”");
+
+    //misma lógica que con IPv6
+    char miiptext [INET6_ADDRSTRLEN ] ;
+    if ( inet_ntop ( AF_INET6 , (const void * ) &ipv6 , miiptext , INET6_ADDRSTRLEN) != NULL) {
+        printf ( "%s \n" , miiptext ) ; 
+    }
+     
+}
